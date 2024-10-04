2ee0f4c2f559d548cc8bdf5ed6c1a8bf2e6e4920
bv-hemant <hemant@blogvault.net>
2024-10-04 13:09:20 +0000

    Fix #3: Ability to modify redis port

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,3 +3,7 @@ require './config/initializers/config'
 Sidekiq.configure_server do |config|
   config.redis = { url: CONFIG['redis_url'] }
 end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: CONFIG['redis']['url'] }
+end
