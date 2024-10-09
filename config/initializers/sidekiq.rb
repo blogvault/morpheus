061e4185b2af3bb1c3fa4ced38885390134642f4
bv-hemant <hemant@blogvault.net>
2024-10-09 12:11:18 +0000

    Fix #16: Add puma and sidekiq logger

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,14 @@
 require './config/initializers/config'
 
+#LOGGER
+sidekiq_logger = Logger.new('./log/sidekiq.log')
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: CONFIG['redis']['url'] }
+	config.redis = { url: CONFIG['redis']['url'] }
+	config.logger = sidekiq_logger
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: CONFIG['redis']['url'] }
+	config.logger = sidekiq_logger
 end
